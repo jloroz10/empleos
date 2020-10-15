0707ed63f2b3076ae236cd76267ad6b64a844457
jloroz10 <jlorozco.1025@gmail.com>
2020-10-15 00:31:32 -0400

    adding requestmappings

--- src/main/java/com/dylproject/controller/HomeController.java
+++ src/main/java/com/dylproject/controller/HomeController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.dylproject.model.Vacante;
 
@@ -123,4 +124,6 @@ public class HomeController {
 		
 		return "home"; //home is the name of the template (home.html) saved in src/main/resources/templates
 	}
+	
+
 }
